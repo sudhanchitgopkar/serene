a460204d1995ebba9ff3a70105f078d7eeed72c3
luked <lcd67420@uga.edu>
2023-02-04 14:52:16 -0500

    fixed var usage in building url

--- public/javascript/Security.js
+++ public/javascript/Security.js
@@ -41,11 +41,10 @@ const Security = class {
     // returns the price information from Alpha Vantage in a json
     queryPriceHistory(ticker) {
         console.groupCollapsed("Anything")
-        let symbol = "META";
         let apikey = "8GJ0ZCRX0UGW3NPC";
 
         let url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED"
-        url += "&symbol=" + symbol;
+        url += "&symbol=" + ticker;
         url += "&datatype=" + "json";
         url += "&apikey=" + apikey;
 
