97d4bac4e1305845bcc11b9ef216ab506a4e1e7e
luked <lcd67420@uga.edu>
2023-02-04 21:38:53 -0500

    added volatility scanner for a user's holdings

--- public/javascript/User.js
+++ public/javascript/User.js
@@ -1,11 +1,29 @@
 class User{
     holdings;
     accounts;
+    // the square of 10 - risk aversness is the threshold
+    volatilityThreshold = Math.pow(10 - 7, 2);
 
     constructor(holdings, accounts) {
         this.setHoldings(holdings);
         this.setAccounts(accounts);
     }
+
+    // scans the user's holdings for items with volatility above the threshhold
+    // returns a list of the holdings that are deemed too volatile for the user
+    getOverlyVolatileHoldings() {
+        var volatileHoldings;
+        // iterate through holdings
+        for (var holding in this.holdings) {
+            if (this.holdings[holding].volatility != -1 &&
+                // * 10 since we want to compare as percents
+                this.holdings[holding].volatility * 10 >= volatilityThreshold) {
+                // volatile stock found
+                volatileHoltings.push(holdings[holdings]);
+            } // if
+        } // for holding
+        return volatileHoldings;
+    } // getOverlyVolatileHoldings()
 
     getHoldings() {
         return this.holdings;
